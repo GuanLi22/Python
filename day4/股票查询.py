70456d9e74f4e69953cb2f9e08bbcf7303bfa2fa
1222165+guan-li22@user.noreply.gitee.com <727299572@qq.com>
2023-01-12 07:30:39 +0800

    1.11补

--- day4/股票查询.py
+++ day4/股票查询.py
@@ -2,6 +2,8 @@
 # @Time : 2023/1/10 9:11
 # @Author : GuanLi
 
+import re
+
 # 股票查询要求：
 # 开发程序对 stock data.txt 进行以下操作:
 
@@ -31,10 +33,44 @@ while True:  #允许用户无限的输入信息
 # 3.允许按当前价、涨跌幅、换手率这几列来筛选信息，比如输入“当前价>50"则把价格大于50的股票都打印，
 #   输入"涨跌幅<50"，则把涨跌幅小于50的股票都打印，不用判断等于。
 
+
 #这叫做公式查询
 # 1.公式的合法性判断 -----正则表达式
+    cmd_paser = re.split("[<>]", cmd) #切这个用户输入的指令
+    if len(cmd_paser) != 2:  #如果输入的公式不合法,切出来长度就不是2
+        print("指令无效")
+        continue
 
 # 2.判断列名的合法性
+    filter_column, filter_var = cmd_paser
+    filter_column = filter_column.strip(" ")
+    filter_var = filter_var.strip("%")  #去掉输入时的%
+    if filter_column not in ["当前价", "涨跌幅", "换手率"]:
+        print("查询对象无效")
+        continue
+
+
 # 3.判断数值的合法性
+    try:
+        filter_var = float(filter_var)
+
+    except:
+
+        print("查询数值无效")
+        continue
+
+# try中运行的代码如果有问题不要报错，运行except中的代码
+
+
 # 4.处理逻辑
 #   1)索引 ------ headers
+    column_index = headers.index(filter_column) #返回输入的对象(列名)的索引
+    for s_id, s_val in stock_dict.items():
+        if ">" in cmd:
+            if float(s_val[column_index].strip("%")) > filter_var: #去掉要比对的文件中的%
+                print(s_val)
+        else:
+            if float(s_val[column_index].strip("%")) < filter_var:
+                print(s_val)
+
+
